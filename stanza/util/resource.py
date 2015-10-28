bf83f3110f3a38e711c2f0edbf74b7d727eea300
Victor Zhong <victor@victorzhong.com>
2015-10-27 23:06:30 -0700

    minor fix: do not use reversed keyword 'dir' as variable name

--- stanza/util/resource.py
+++ stanza/util/resource.py
@@ -7,11 +7,11 @@ def get_from_url(url):
     return requests.get(url).content
 
 def get_data_or_download(dir_name, file_name, url=''):
-    dir = os.path.join(stanza.DATA_DIR, dir_name)
-    fname = os.path.join(dir, file_name)
-    if not os.path.isdir(dir):
-        assert url, 'Could not locate data {}, and url was not specified. Cannot retrieve data.'.format(dir)
-        os.makedirs(dir)
+    dname = os.path.join(stanza.DATA_DIR, dir_name)
+    fname = os.path.join(dname, file_name)
+    if not os.path.isdir(dname):
+        assert url, 'Could not locate data {}, and url was not specified. Cannot retrieve data.'.format(dname)
+        os.makedirs(dname)
     if not os.path.isfile(fname):
         assert url, 'Could not locate data {}, and url was not specified. Cannot retrieve data.'.format(fname)
         print('downloading from {}'.format(url))
