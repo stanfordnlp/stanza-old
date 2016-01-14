ca8b0aefd8d1b3ca0ad7de8ce7323ff7e667e732
Victor Zhong <victor@victorzhong.com>
2016-01-14 22:29:52 +0000

    adding command to run all tests

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ class UnitTest(Command):
         errno = subprocess.call(['python', '-m', 'unittest', 'discover', 'stanza/test/unit_tests'])
         raise SystemExit(errno)
 
+
 class SlowTest(Command):
     user_options = []
 
@@ -31,6 +32,22 @@ class SlowTest(Command):
         errno = subprocess.call(['python', '-m', 'unittest', 'discover', 'stanza/test/slow_tests'])
         raise SystemExit(errno)
 
+
+class AllTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        errno = subprocess.call(['python', '-m', 'unittest', 'discover', 'stanza/test'])
+        raise SystemExit(errno)
+
+
 setup(
     name='stanza',
     version='0.1',
@@ -40,7 +57,7 @@ setup(
     author='Stanford NLP',
     author_email='victor@victorzhong.com',
     description='NLP library for Python',
-    cmdclass={'test': UnitTest, 'slow_test': SlowTest},
+    cmdclass={'test': UnitTest, 'slow_test': SlowTest, 'all_test': AllTest},
     download_url='https://github.com/stanfordnlp/stanza/tarball/0.1',
     keywords=['nlp', 'neural networks', 'machine learning'],
 )
