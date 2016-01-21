c04e0f97435fe3d3796d636a172efa6c8c54960b
Kelvin Guu <Kelvin@MacBook-Pro-63.local>
2016-01-21 11:44:35 -0800

    updated tests to reflect new Vocab implem
    All tests pass

--- stanza/test/unit_tests/text/test_vocab.py
+++ stanza/test/unit_tests/text/test_vocab.py
@@ -5,64 +5,52 @@ from stanza.text.vocab import Vocab, SennaVocab
 
 
 class TestVocab(TestCase):
-    
+
     def setUp(self):
         self.Vocab = Vocab
 
     def test_unk(self):
-        v = self.Vocab(unk='') # no unk
-        self.assertEqual(len(v), 0)
-
-        v = self.Vocab(unk='**UNK**')
+        unk = '**UNK**'
+        v = self.Vocab(unk=unk)
         self.assertEqual(len(v), 1)
-        self.assertIn(v.unk, v)
-        self.assertEqual(v[v.unk], 0)
-        self.assertEqual(v.counts[v.unk], 0)
-        self.assertEqual(v.unk, '**UNK**')
+        self.assertIn(unk, v)
+        self.assertEqual(v[unk], 0)
+        self.assertEqual(v.counts[unk], 0)
 
     def test_add(self):
-        v = self.Vocab('')
+        v = self.Vocab('**UNK**')
         v.add('hi')
         self.assertIn('hi', v)
-        self.assertEqual(len(v), 1)
+        self.assertEqual(len(v), 2)
         self.assertEqual(v.counts['hi'], 1)
-        self.assertEqual(v['hi'], 0)
+        self.assertEqual(v['hi'], 1)
 
     def test_sent2index(self):
         v = self.Vocab(unk='unk')
-        inds = v.words2indices(['i', 'like', 'pie'], add=True)
-        self.assertEqual(inds, [1, 2, 3])
-        inds = v.words2indices(['i', 'said'], add=True)
-        self.assertEqual(inds, [1, 4])
-        inds = v.words2indices(['you', 'said'], add=False)
-        self.assertEqual(inds, [0, 4])
+        words = ['i', 'like', 'pie']
+        v.update(words)
+        self.assertEqual(v.words2indices(words), [1, 2, 3])
+        self.assertEqual(v.words2indices(['i', 'said']), [1, 0])
 
     def test_index2sent(self):
         v = self.Vocab(unk='unk')
-        inds = v.words2indices(['i', 'like', 'pie'], add=True)
+        v.update(['i', 'like', 'pie'])
         words = v.indices2words([1, 2, 3, 0])
-        self.assertEqual(words, ['i', 'like', 'pie', v.unk])
+        self.assertEqual(words, ['i', 'like', 'pie', 'unk'])
 
     def test_prune_rares(self):
         v = self.Vocab(unk='unk')
-        v.words2indices(['hi'] * 3 + ['bye'] * 5, add=True)
-        self.assertEqual({'hi': 3, 'bye': 5, v.unk: 0}, v.counts)
+        v.update(['hi'] * 3 + ['bye'] * 5)
+        self.assertEqual({'hi': 3, 'bye': 5, 'unk': 0}, dict(v.counts))
         p = v.prune_rares(cutoff=4)
-        self.assertEqual({'bye': 5, v.unk: 0}, p.counts)
+        self.assertEqual({'bye': 5, 'unk': 0}, dict(p.counts))
 
     def test_sort_by_decreasing_count(self):
         v = self.Vocab(unk='unk')
-        v.words2indices('some words words for for for you you you you'.split(), add=True)
+        v.update('some words words for for for you you you you'.split())
         s = v.sort_by_decreasing_count()
-        self.assertEqual(['unk', 'you', 'for', 'words', 'some'], s.index2word)
-        self.assertEqual({'unk': 0, 'you': 4, 'for': 3, 'words': 2, 'some': 1}, s.counts)
-
-        # same test, no unk
-        v = self.Vocab('')
-        v.words2indices('some words words for for for you you you you'.split(), add=True)
-        s = v.sort_by_decreasing_count()
-        self.assertEqual(['you', 'for', 'words', 'some'], s.index2word)
-        self.assertEqual({'you': 4, 'for': 3, 'words': 2, 'some': 1}, s.counts)
+        self.assertEqual(['unk', 'you', 'for', 'words', 'some'], list(iter(s)))
+        self.assertEqual({'unk': 0, 'you': 4, 'for': 3, 'words': 2, 'some': 1}, dict(s.counts))
 
 
 class TestSenna(TestVocab):
