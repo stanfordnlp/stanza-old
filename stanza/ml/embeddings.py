cf4c6a60119eaa4dbe607a31c286f5efe61d0b92
Kelvin Guu <guu.kelvin@gmail.com>
2016-06-17 22:16:38 -0700

    a simple Embeddings object
    bundles an embedding matrix and a vocab
    offers efficient saving and loading

---
+++ stanza/ml/embeddings.py
@@ -0,0 +1,54 @@
+__author__ = 'kelvinguu'
+
+from contextlib import contextmanager
+import logging
+import numpy as np
+from stanza.text import Vocab
+
+
+class Embeddings(object):
+
+    def __init__(self, array, vocab):
+        """Create embeddings object.
+
+        :param array: a numpy array
+        :param vocab: a Vocab object
+        """
+        assert isinstance(array, np.ndarray)
+        assert isinstance(vocab, Vocab)
+        assert array.shape[0] == len(vocab)  # entries line up
+        self.array = array
+        self.vocab = vocab
+
+    def to_files(self, array_file, vocab_file):
+        logging.info('Writing array...')
+        np.save(array_file, self.array)
+        logging.info('Writing vocab...')
+        self.vocab.to_file(vocab_file)
+
+    @classmethod
+    def from_files(cls, array_file, vocab_file):
+        logging.info('Loading array...')
+        array = np.load(array_file)
+        logging.info('Loading vocab...')
+        vocab = Vocab.from_file(vocab_file)
+        return cls(array, vocab)
+
+    @staticmethod
+    @contextmanager
+    def path_prefix_to_files(path_prefix, mode):
+        array_path = path_prefix + '.npy'
+        vocab_path = path_prefix + '.vocab'
+        print 'Starting'
+        with open(array_path, mode) as array_file, open(vocab_path, mode) as vocab_file:
+            yield array_file, vocab_file
+        print 'Done'
+
+    def to_file_path(self, path_prefix):
+        with self.path_prefix_to_files(path_prefix, 'w') as (array_file, vocab_file):
+            self.to_files(array_file, vocab_file)
+
+    @classmethod
+    def from_file_path(self, path_prefix):
+        with self.path_prefix_to_files(path_prefix, 'r') as (array_file, vocab_file):
+            return self.from_files(array_file, vocab_file)
